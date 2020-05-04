bc245d47a24909d2894d74cf5fc57bdda78228c6
Carson Keller <carsonjkeller@gmail.com>
2020-05-04 16:18:47 -0600

    Updated used file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Game from './Game.js';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
