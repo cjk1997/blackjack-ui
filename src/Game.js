a9f91b2e917b0f9338adad44d10cfad99249e3ad
Carson Keller <carsonjkeller@gmail.com>
2020-05-05 11:08:19 -0600

    Corrected state variable references

--- src/Game.js
+++ src/Game.js
@@ -42,71 +42,71 @@ class Game extends Component {
         for (let i = 1; 1 <= numPlayers; i++) {
             let hand = new Array();
             let player = { name : "Player" + i, ID : i, points : 0, hand : hand };
-            players.push(player);
+            this.state.players.push(player);
         };
     };
 
     getPoints = (player) => {
         let points = 0;
-        for (let i = 0; i < players[player].hand.length - 1; i++) {
-            points += players[player].hand[i].value
+        for (let i = 0; i < this.state.players[player].hand.length - 1; i++) {
+            points += this.state.players[player].hand[i].value
         };
-        players[player].points = points;
+        this.state.players[player].points = points;
         return points;
     };
 
     updatePoints = () => {
-        for (let i = 0; i < players.length; i++) {
+        for (let i = 0; i < this.state.players.length; i++) {
             this.getPoints(i);
-            document.getElementById('player_' + i).innerHTML = players[i].points;
+            document.getElementById('player_' + i).innerHTML = this.state.players[i].points;
         };
     };
 
     dealCards = () => {
         for (let i = 0; i < 2; i++) {
-            for (let j = 0; j < players.length; j++) {
-                let card = cardDeck.pop();
-                players[j].hand.push(card);
-                renderCard(card, j);
-                updatePoints();
+            for (let j = 0; j < this.state.players.length; j++) {
+                let card = this.state.cardDeck.pop();
+                this.stateplayers[j].hand.push(card);
+                this.renderCard(card, j);
+                this.updatePoints();
             };
         };
-        updateDeck();
+        this.updateDeck();
     };
 
     renderCard = (card, player) => {
-        let hand = getElementById('hand_' + player);
-        hand.appendChild(addCard(card));
+        let hand = document.getElementById('hand_' + player);
+        hand.appendChild(this.addCard(card));
     };
 
     addCard = (card) => {
-        let add = getElementByClassName('card');
+        let add = document.getElementByClassName('card');
         add.innerHTML = card.name + ' of ' + card.suit;
         return add;
     };
 
     hitMe = () => {
-        let card = deck.pop();
-        players[this.state.currentPlayer].hand.push(card);
-        renderCard(card, this.state.currentPlayer);
-        updatePoints();
-        updateDeck();
-        check();
+        let card = this.state.cardDeck.pop();
+        this.state.players[this.state.currentPlayer].hand.push(card);
+        this.renderCard(card, this.state.currentPlayer);
+        this.updatePoints();
+        this.updateDeck();
+        this.check();
     };
 
     check = () => {
-        if (players[this.state.currentPlayer].points > 21) {
-            document.getElementById('status').innerHTML = 'Player: ' + players[this.state.currentPlayer].ID + ' LOST'
+        if (this.state.players[this.state.currentPlayer].points > 21) {
+            document.getElementById('status').innerHTML = 'Player: ' + this.state.players[this.state.currentPlayer].ID + ' LOST'
         };
     };
 
     stay = () => {
-        if (this.state.currentPlayer != players.length - 1) {
+        if (this.state.currentPlayer !== this.state.players.length - 1) {
             document.getElementById('player_' + this.state.currentPlayer).classList.remove('active');
             this.state.currentPlayer += 1;
             document.getElementById('player_' + this.state.currentPlayer).classList.add('active');
         } else {
-            end();
+            this.end();
         };
     };
 
@@ -114,33 +114,33 @@ class Game extends Component {
         let winner = -1;
         let score = 0;
 
-        for (let i = 0; i < players.length - 1; i++){
-            if (players[i].points > score && players[i].points < 22) {
+        for (let i = 0; i < this.state.players.length - 1; i++){
+            if (this.state.players[i].points > score && this.state.players[i].points < 22) {
                 winner = i;
             };
-            score = players[i].points;
+            score = this.state.players[i].points;
         };
-        document.getElementById('status').innerHTML = "Winner: Player " + players[winner].id;
+        document.getElementById('status').innerHTML = "Winner: Player " + this.state.players[winner].id;
     };
 
     check = () => {
-        if (players[currentPlayer].points > 21) {
-            document.getElementById('status').innerHTML = 'Player: ' + players[currentPlayer].ID + ' LOST';
+        if (this.state.players[this.state.currentPlayer].points > 21) {
+            document.getElementById('status').innerHTML = 'Player: ' + this.state.players[this.state.currentPlayer].ID + ' LOST';
         };
     };
 
     updateDeck = () => {
-        document.getElementById('deckcount').innerHTML = cardDeck.length;
+        document.getElementById('deckcount').innerHTML = this.state.cardDeck.length;
     };
 
     render() {
         const renderPlayers = this.state.players.map((player) => {
             return(
                 <>
-                    <div className="player" id={"player_" + players.findIndex(player)} value={player.name}>
-                        <div className="playerID" value={players.findIndex(player).ID}/>
-                        <div className="hand" id={"hand_" + players.findIndex(player)} value={player.hand}/>
-                        <div className="points" id={"points_" + players.findIndex(player)} value={player.points}/>
+                    <div className="player" id={"player_" + this.state.players.findIndex(player)} value={player.name}>
+                        <div className="playerID" value={this.state.players.findIndex(player).ID}/>
+                        <div className="hand" id={"hand_" + this.state.players.findIndex(player)} value={player.hand}/>
+                        <div className="points" id={"points_" + this.state.players.findIndex(player)} value={player.points}/>
                     </div>
                 </>
             );
@@ -152,9 +152,9 @@ class Game extends Component {
                     <div className="card"/>
                     <div className="gameBody">
                         <div className="gameButtons">
-                            <input type="button" className="buttons" id="startbutton" value="Start" onClick={}/>
-                            <input type="button" className="buttons" id="hitMeButton" value="Hit Me!" onClick={}/>
-                            <input type="button" className="buttons" id="stayButton" value="Stay" onClick={}/>
+                            <input type="button" className="buttons" id="startbutton" value="Start" onClick={this.startGame}/>
+                            <input type="button" className="buttons" id="hitMeButton" value="Hit Me!" onClick={this.hitMe}/>
+                            <input type="button" className="buttons" id="stayButton" value="Stay" onClick={this.stay}/>
                         </div>
                         <div className="status" id="status"/>
                         <div className="players" id="players">
